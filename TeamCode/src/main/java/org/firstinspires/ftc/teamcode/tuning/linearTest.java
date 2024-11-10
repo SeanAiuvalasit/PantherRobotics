98c864445f93a2ed3e0215abfdf5932ea1f35af2
pascuals_triangle <pascualstriangle@gmail.com>
2024-11-09 22:21:00 -0600

    joystick encoder control and given position for slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/linearTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/linearTest.java
@@ -1,3 +1,11 @@
+/**
+ * User Controls:
+ * Connect gamepad as user 1
+ * left stick = rotation (+y rotates up)
+ * right stick = slide (+y extends out)
+ * b = go to slide target position
+ */
+
 package org.firstinspires.ftc.teamcode.tuning;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -16,22 +24,36 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.JavaUtil;
 
 @TeleOp(name = "linearTest")
-public class linearTest extends LinearOpMode {
-    //Hware hware = new Hware(hardwareMap);
-    //DcMotor motor;
-    //tick value - 537.7
-    //5,281.1 - new tick value
-    final double TPR = 5281.1; // ticks per revolution
-    final double POSITION_TOLERANCE = 0.05; // prevents infinite loop
-    final int SLIDE_MAX = 3089;
-    double revolutions = 0.0; // used for calculation of rotation angle
-    double angle = 0.0; // degrees
-    double slideTarget = 0.0; // target position (0-1)
-    int leftTarget, rightTarget, leftSlideTarget, rightSlideTarget,
-        leftPos, rightPos, leftSlidePos, rightSlidePos; // all corrected for direction/scale
-
+public class linearTest extends LinearOpMode
+{
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException
+    {
+        //Hware hware = new Hware(hardwareMap);
+        //DcMotor motor;
+        //tick value - 537.7
+        //5,281.1 - new tick value
+        final double TPR = 5281.1;                  // ticks per revolution
+        final double POSITION_TOLERANCE = 0.05;     // prevents infinite loop
+        // TODO: update target position
+        final double SLIDE_TARGET = 0.5;            // slide position to go to on button press
+        final int SLIDE_MAX = 3089;                 // empirical position of fully extended arm
+        // TODO: update max angle
+        final int MAX_ANGLE = 90;                   // degrees
+
+        double revolutions = 0.0;                   // used for calculation of rotation angle
+        double angle = 0.0;                         // degrees
+        double slideTarget = 0.0;                   // target position (0-1)
+        // all of the below are in revolutions, corrected for direction
+        int leftTarget, rightTarget, leftSlideTarget, rightSlideTarget;
+        int leftPos = 0;
+        int rightPos = 0;
+        int leftSlidePos = 0;
+        int rightSlidePos = 0;
+
+        boolean lastCycle = false;                  // used with below to read button presses properly
+        boolean thisCycle = false;
+
         DcMotor leftSlide = hardwareMap.get(DcMotorEx.class, "leftSlide");
         DcMotor rightSlide = hardwareMap.get(DcMotorEx.class, "rightSlide");
         DcMotor left = hardwareMap.get(DcMotorEx.class, "left");
@@ -48,7 +70,6 @@ public class linearTest extends LinearOpMode {
         rightSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-
         left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         left.setDirection(DcMotor.Direction.FORWARD);
         left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -62,39 +83,37 @@ public class linearTest extends LinearOpMode {
 
         waitForStart();
 
-        while(opModeIsActive()){
+        while(opModeIsActive())
+        {
             // updates with current position
-            telemetry.addData("pos", slideTarget );
+            telemetry.addData("rotation target", angle + " degrees");
+            telemetry.addData("rotation position", rightPos + " degrees");
+            telemetry.addData("rotation power", right.getPower());
+            telemetry.addData("slide target", slideTarget + " rotations");
+            telemetry.addData("slide position", rightSlidePos / 360 + " rotations");
+            telemetry.addData("slide power", rightSlide.getPower());
+            telemetry.addData("left joystick", gamepad1.left_stick_y);
+            telemetry.addData("right joystick", gamepad1.right_stick_y);
             telemetry.update();
 
             // revolutions used to compare target to pos - easier to convert
             // angle used for user input - easier to visualize
             // gear ratio of 3
-            angle = 90;
-            revolutions = angle / 360 / 3;
+            revolutions = (angle / 360) / 3;
 
             // calculate target for given angle
             leftTarget = (int) (-1 * revolutions * TPR);
             rightTarget = (int) (revolutions * TPR);
-            leftSlideTarget = (int) (-1 * slideTarget * SLIDE_MAX);
-            rightSlideTarget = (int) (slideTarget * SLIDE_MAX);
 
             // record current positions
             leftPos = (int) (-1 * left.getCurrentPosition() % TPR);
             rightPos = (int) (right.getCurrentPosition() % TPR);
-            leftSlidePos = (int) (-1 * leftSlide.getCurrentPosition() / SLIDE_MAX);
-            rightSlidePos = (int) (rightSlide.getCurrentPosition() / SLIDE_MAX);
 
             // set motor targets and powers
             left.setTargetPosition(leftTarget);
             right.setTargetPosition(rightTarget);
-            leftSlide.setTargetPosition(leftSlideTarget);
-            rightSlide.setTargetPosition(rightSlideTarget);
-
-            left.setPower(0.1);
-            right.setPower(0.1);
-            leftSlide.setPower(0.15);
-            rightSlide.setPower(0.15);
+            left.setPower(-0.25);
+            right.setPower(0.25);
 
             // if not in correct position, run rotation to that position
             if (Math.abs(leftTarget-leftPos) > POSITION_TOLERANCE)
@@ -103,56 +122,48 @@ public class linearTest extends LinearOpMode {
                 right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
 
-            // if not in correct position, run slide to that position
-            if (Math.abs(leftSlideTarget-leftPos) > POSITION_TOLERANCE)
+            // left stick changes target angle (0-MAX_ANGLE)
+            if (angle > MAX_ANGLE)
+                angle = MAX_ANGLE;
+            else if (angle < 0)
+                angle = 0;
+            else if (gamepad1.left_stick_y > 0.5 || gamepad1.left_stick_y < -0.5)
+                angle -= gamepad1.left_stick_y;
+
+            // press b to go to position 0.5
+            lastCycle = thisCycle;
+            thisCycle = gamepad1.b;
+            if (!lastCycle && thisCycle)
+                slideTarget = SLIDE_TARGET;
+
+            // calculate target for given angle
+            leftSlideTarget = (int) (-1 * slideTarget * SLIDE_MAX);
+            rightSlideTarget = (int) (slideTarget * SLIDE_MAX);
+
+            // record current positions
+            leftSlidePos = (int) (-1 * leftSlide.getCurrentPosition() / SLIDE_MAX);
+            rightSlidePos = (int) (rightSlide.getCurrentPosition() / SLIDE_MAX);
+
+            // set motor targets and powers
+            leftSlide.setTargetPosition(leftSlideTarget);
+            rightSlide.setTargetPosition(rightSlideTarget);
+            leftSlide.setPower(-0.5);
+            rightSlide.setPower(0.5);
+
+            // if not in correct position, run rotation to that position
+            if (Math.abs(leftSlideTarget-leftSlidePos) > POSITION_TOLERANCE)
             {
                 leftSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 rightSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
 
-            /*
-            // dpad up increases angle, dpad down decreases angle
-            // angle must be within 0 and 90 degrees
-            if (angle > 90)
-            {
-                angle = 90;
-            }
-            else if (angle < 0)
-            {
-                angle = 0;
-            }
-            else
-            {
-                if (gamepad1.dpad_up)
-                {
-                    angle++;
-                }
-                else if (gamepad1.dpad_down)
-                {
-                    angle--;
-                }
-            }
-             */
-
+            // left stick changes target angle (0-MAX_ANGLE)
             if (slideTarget > 1)
-            {
                 slideTarget = 1;
-            }
             else if (slideTarget < 0)
-            {
                 slideTarget = 0;
-            }
-            else
-            {
-                if (gamepad1.y)
-                {
-                    slideTarget += 0.005;
-                }
-                else if (gamepad1.a)
-                {
-                    slideTarget -= 0.005 ;
-                }
-            }
+            else if (gamepad1.right_stick_y > 0.5 || gamepad1.right_stick_y < -0.5)
+                slideTarget -= (0.01 *  gamepad1.right_stick_y);
         }
     }
 }
