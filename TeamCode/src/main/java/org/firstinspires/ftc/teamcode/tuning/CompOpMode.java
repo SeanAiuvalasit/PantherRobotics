89615c2ab07fe1a6e04e6a5c52414ae1766de385
HRNAE <148918405+HRNAE@users.noreply.github.com>
2024-11-09 19:18:13 -0600

    mecanum drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/CompOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/CompOpMode.java
@@ -1,10 +1,13 @@
-/*
+
 package org.firstinspires.ftc.teamcode.tuning;
 
 //package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+@TeleOp(name = "MecanumTest")
 public class CompOpMode extends LinearOpMode {
 
 
@@ -12,11 +15,58 @@ public class CompOpMode extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         double drive, turn, strafe;
         double fLeftPower, fRightPower, bLeftPower, bRightPower;
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "LF");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "RF");
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "LB");
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "RB");
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        drive - left joystick y axis
-        turn - right joystick x axis
-        strafe - left joystick x axis
+        waitForStart();
+
+        while (opModeIsActive()) {
+            drive = gamepad1.left_stick_y * -1;
+            turn = gamepad1.right_stick_x;
+            strafe = gamepad1.left_stick_x;
+
+            //strafe
+            fLeftPower = drive + turn + strafe;
+            fRightPower = drive - turn - strafe;
+            bLeftPower = drive + turn - strafe;
+            bRightPower = drive - turn + strafe;
+
+            double[] appliedPowers = scalePowers(fLeftPower, fRightPower, bLeftPower, bRightPower);
+
+            leftFront.setPower(appliedPowers[0]);
+            leftBack.setPower(appliedPowers[2]);
+            rightFront.setPower(appliedPowers[1]);
+            rightBack.setPower(appliedPowers[3]);
+        }
 
     }
+
+    public double[] scalePowers(double fLeftPower, double fRightPower, double bLeftPower, double bRightPower) {
+        double max = Math.max(Math.abs(fLeftPower), Math.max(Math.abs(fRightPower), Math.max(Math.abs(bLeftPower), Math.abs(bRightPower))));
+        if (max > 1) {
+            fLeftPower /= max;
+            fRightPower /= max;
+            bLeftPower /= max;
+            bRightPower /= max;
+        }
+
+        double[] motorPowers = new double[]{fLeftPower, fRightPower, bLeftPower, bRightPower};
+        return motorPowers;
+
+    }
+
 }
-*/
